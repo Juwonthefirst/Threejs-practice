a72c73f48ff6df90b436baade8fdcc618e3e8e73
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-06-09 22:14:41 +0100

    update webpack.config.js

---
+++ 04-webpack/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	mode: 'production',
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	plugins: [
+		new htmlWebpackPlugin({template: './src/template.html'}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"]
+			},
+			{
+				test: /\.html$/i,
+				use: "html-loader",
+			}
+		]
+	}
+}
