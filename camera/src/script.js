82990b84dbebcaf4e686793b93ef1660ad9c9c1a
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-06-12 19:55:03 +0100

    update script.js

--- camera/src/script.js
+++ camera/src/script.js
@@ -5,12 +5,34 @@ import * as THREE from '../../cdn_modules/three.js@0.174.0/three.module.js'
 /**
  * Base
  */
+console.log(gsap)
+const touchCoordinates = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('touchmove', (event) => {
+    const touch = event.touches[0]
+    touchCoordinates.x = touch.clientX / sizes.width - 0.5
+    touchCoordinates.y = touch.clientY / sizes.height - 0.5
+    console.log(touchCoordinates)
+})
+
+window.addEventListener('touchend', () => {
+    console.log('touchend')
+    gsap.to(touchCoordinates, { duration: 1, x: 0, y: 0 })
+})
+
+window.addEventListener('mousemove', (event) => {
+    touchCoordinates.x = event.clientX / sizes.width - 0.5
+    touchCoordinates.y = event.clientY / sizes.height - 0.5
+})
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Sizes
 const sizes = {
-    width: 800,
+    width: 300,
     height: 600
 }
 
@@ -26,9 +48,9 @@ scene.add(mesh)
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+//camera.position.x = 2
+//camera.position.y = 2
+camera.position.z = 5
 camera.lookAt(mesh.position)
 scene.add(camera)
 
@@ -43,14 +65,16 @@ const clock = new THREE.Clock()
 
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
-
+    /*const elapsedTime = clock.getElapsedTime()
+    
     // Update objects
-    mesh.rotation.y = elapsedTime;
-
+    mesh.rotation.y = elapsedTime;*/
+    
+    camera.position.x = touchCoordinates.x * -5
+    camera.position.y = touchCoordinates.y * 5
     // Render
     renderer.render(scene, camera)
-
+    
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
