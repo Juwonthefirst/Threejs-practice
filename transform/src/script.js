79f8467f355b530fd00599865f12a52dd589c3a9
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-06-12 14:24:30 +0100

    update

--- transform/src/script.js
+++ transform/src/script.js
@@ -1,6 +1,6 @@
-import './style.css'
-import * as THREE from 'three'
-//import * as THREE from '../../cdn_modules/three.js@0.174.0/three.module.js'
+//import './style.css'
+//import * as THREE from 'three'
+import * as THREE from '../../cdn_modules/three.js@0.174.0/three.module.js'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -13,29 +13,29 @@ const scene = new THREE.Scene()
  * Objects
  */
 const group = new THREE.Group()
-group.scale.y = 4
+//group.scale.y = 4
 group.rotation.y = -2
 scene.add(group)
 
 const mesh1 = new THREE.Mesh(
- new THREE.BoxGeometry(1, 1, 1),
- new THREE.MeshBasicMaterial({ color: 0xff0000 })
+	new THREE.SphereGeometry(),
+	new THREE.MeshBasicMaterial({ color: 0xff0000 })
 )
 mesh1.position.x = 0
 mesh1.rotation.reorder('YZX')
 group.add(mesh1)
 
 const mesh2 = new THREE.Mesh(
- new THREE.BoxGeometry(1, 1, 1),
- new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+	new THREE.BoxGeometry(1, 1, 1),
+	new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 )
 mesh2.position.x = -2
 mesh2.rotation.reorder('YZX')
 group.add(mesh2)
 
 const mesh3 = new THREE.Mesh(
- new THREE.BoxGeometry(1, 1, 1),
- new THREE.MeshBasicMaterial({ color: 0x0000ff })
+	new THREE.BoxGeometry(1, 1, 1),
+	new THREE.MeshBasicMaterial({ color: 0x0000ff })
 )
 mesh3.position.x = 2
 mesh3.rotation.reorder('YZX')
@@ -43,26 +43,32 @@ group.add(mesh3)
 
 
 const axis = new THREE.AxesHelper(3)
-scene.add(axis)
+//scene.add(axis)
 /**
  * Sizes
  */
 const sizes = {
-    width: 300,
-    height: 600
+	width: 300,
+	height: 600
 }
 
 /**
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.set( 0, 0, 5 )
-camera.lookAt(group.position)
+camera.position.set(0, 0, 10)
+group.lookAt(camera.position)
 scene.add(camera)
 
 /**
  * Renderer
  */
-const renderer = new THREE.WebGLRenderer({canvas: canvas})
+const renderer = new THREE.WebGLRenderer({ canvas: canvas })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+
+setInterval(() => {
+	group.rotateZ(10)
+	renderer.render(scene, camera)
+}, 1 / 60 * 1000)
