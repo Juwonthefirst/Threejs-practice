d9d44637456a0346226ee816742d00b3e4d1c846
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-06-19 20:21:48 +0100

    update script.js

--- text/src/script.js
+++ text/src/script.js
@@ -1,13 +1,19 @@
-import './style.css'
+/*import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'lil-gui'
+import * as dat from 'lil-gui'*/
+
+import * as THREE from '../../modules/three.module.js'
+import { OrbitControls } from '../../modules/orbit_controls.js'
+import GUI from '../../modules/lil-gui.js'
+import { FontLoader } from '../../modules/Fontloader.js';
+import { TextGeometry } from '../../modules/TextGeometry.js';
 
 /**
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+const gui = new GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -19,7 +25,44 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('/text/static/textures/matcaps/3.png')
 
+/**
+* Fonts
+*/
+const material = new THREE.MeshMatcapMaterial({matcap: matcapTexture})
+const fontLoader = new FontLoader()
+
+fontLoader.load(
+	'/text/static/textures/fonts/helvetiker_regular.typeface.json',
+	(font) => {
+		const textGeometry = new TextGeometry(
+			'Hello World',
+			{
+				font,
+				size: 0.5,
+				height: 0.2,
+				depth: 0.2,
+				curveSegments: 4,
+				bevelEnabled: true,
+				bevelThickness: 0.03,
+				bevelSize: 0.02,
+				bevelOffset: 0,
+				bevelSegments: 2
+			}
+		);
+		/*textGeometry.computeBoundingBox()
+		textGeometry.translate(
+			-(textGeometry.boundingBox.max.x - 0.02) * 0.5,
+			-(textGeometry.boundingBox.max.y - 0.02)* 0.5,
+			-(textGeometry.boundingBox.max.z - 0.03)* 0.5
+		)
+		console.log(textGeometry.boundingBox)*/
+		textGeometry.center()
+		const text = new THREE.Mesh(textGeometry, material)
+		scene.add(text)
+	}
+)
 /**
  * Object
  */
@@ -27,8 +70,27 @@ const cube = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshBasicMaterial()
 )
-
-scene.add(cube)
+const shape = Math.random()
+const randomShape = shape < 0.5? new THREE.TorusGeometry(0.3, 0.2, 20, 45) : new THREE.BoxGeometry(1, 1, 1, 16, 16)
+//scene.add(cube)
+console.time('donut loop')
+for (let i = 0; i < 300; i++) {
+	const donut = new THREE.Mesh(
+		randomShape,
+		material
+	)
+	donut.position.x = (Math.random() - 0.5) * 15
+	donut.position.y = (Math.random() - 0.5) * 15
+	donut.position.z = (Math.random() - 0.5) * 15
+	
+	donut.rotation.x = Math.random() * Math.PI
+	donut.rotation.y = Math.random() * Math.PI
+	
+	const scale = Math.random()
+	donut.scale.set(scale, scale, scale)
+	scene.add(donut)
+}
+console.timeEnd('donut loop')
 
 /**
  * Sizes
@@ -37,6 +99,9 @@ const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
+
+const axes = new THREE.AxesHelper(10)
+//scene.add(axes)
 
 window.addEventListener('resize', () =>
 {
@@ -60,7 +125,7 @@ window.addEventListener('resize', () =>
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
 camera.position.y = 1
-camera.position.z = 2
+camera.position.z = 5
 scene.add(camera)
 
 // Controls
@@ -80,11 +145,19 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
+const guiControls = {
+	startAnimation: false
+}
+
+gui
+	.add(guiControls, 'startAnimation')
+	.name('start animation')
+	
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
+	if(guiControls.startAnimation)camera.position.z = Math.cos(elapsedTime) * 5
     // Update controls
     controls.update()
 
